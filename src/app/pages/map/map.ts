ec5be1357c814865eae2681554ea18ef3c83c8f4
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-01 14:29:05 +0200

    Update map.ts

--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -1,3 +1,23 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonAccordionGroup } from '@ionic/angular';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: 'example.component.html',
+})
+export class ExampleComponent {
+  @ViewChild('accordionGroup', { static: true }) accordionGroup: IonAccordionGroup;
+
+  toggleAccordion = () => {
+    const nativeEl = this.accordionGroup;
+    if (nativeEl.value === 'second') {
+      nativeEl.value = undefined;
+    } else {
+      nativeEl.value = 'second';
+    }
+  };
+}
+
 import { Component } from '@angular/core';
 
 import { ActionSheetController } from '@ionic/angular';
